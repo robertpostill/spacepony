48d78d231df46f9011c34a0c0e1d2e4a086117ea
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-10-16 15:14:53 +1100

    Horse needs to lose weight in drastic ways

--- src/js/horse.js
+++ src/js/horse.js
@@ -1,5 +1,5 @@
 function createHorseBody() {
-  var geometry = new THREE.BoxGeometry(2, 1, 5);
+  var geometry = new THREE.BoxGeometry(1.5, 1, 5);
   var material = new THREE.MeshBasicMaterial({color: 0x8C5013});
   var body = new THREE.Mesh(geometry, material);
 
